67c19c86e43805a2d05ca9ba336cbb2a48c98df6
serna <serna@192.168.1.70>
2021-01-15 18:19:01 -0600

    Tarea 1

---
+++ 01Opera.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+   /* 1. Operadores 9 / Nov / 2020 */
+
+int  main ( int argc, char ** argv) {
+   
+    float x = 0 ;
+    
+    x =(6.18/5)*(((14.1 - 4)*(14.1-4))/4);
+    
+    printf ("%f", x);
+    
+    return (0);
+}
